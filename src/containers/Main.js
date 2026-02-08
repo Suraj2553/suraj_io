a110495ec3a9e4c70fe4bd4a9647a6a01fe8655e
Suraj Jha <sjha2102@gmail.com>
2026-02-08 06:17:57 +0530

    Deploy portfolio

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -29,7 +29,7 @@ export default class Main extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/suraj_io">
         <Switch>
           {/* Home */}
           <Route
